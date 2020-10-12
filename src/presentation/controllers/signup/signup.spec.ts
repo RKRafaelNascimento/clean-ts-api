c0467cff67277110cf6483376c130d41bfeae2a8
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-10-12 10:31:30 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,5 +1,14 @@
-import { InvalidParamsError, MissingParamsError, ServerError } from '../../erros'
-import { AccountModel, AddAccount, AddAccountModel, EmailValidator } from './signup-protocols'
+import {
+  InvalidParamsError,
+  MissingParamsError,
+  ServerError
+} from '../../erros'
+import {
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  EmailValidator
+} from './signup-protocols'
 import { SignUpController } from './signUp'
 
 const makeEmailValidator = (): EmailValidator => {
@@ -140,9 +149,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(
-      new InvalidParamsError('email')
-    )
+    expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
   })
 
   it('Should call EmailValidator with correct email', () => {
@@ -181,9 +188,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(
-      new ServerError()
-    )
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 
   it('Should call AddAccount with correct values', () => {
@@ -226,8 +231,28 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(
-      new ServerError()
-    )
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  it('Should return 200 if valid data is provided', () => {
+    const { sut } = makesut()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@gmail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@gmail.com',
+      password: 'valid_password'
+    })
   })
 })
