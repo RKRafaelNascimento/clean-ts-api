8124f346c03a440653ea8f3c4e2fa92750180df5
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-10-23 23:48:11 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/db-account/add-db-account.spec.ts
+++ src/data/usecases/db-account/add-db-account.spec.ts
@@ -1,16 +1,32 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAcount } from './add-db-account'
+
+interface SutTypes {
+  sut: DbAddAcount
+  encrypterSub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncryterStub {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise((resolve) => resolve('hashed_password'))
+    }
+  }
+
+  const encrypterSub = new EncryterStub()
+  const sut = new DbAddAcount(encrypterSub)
+
+  return {
+    sut,
+    encrypterSub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncryterStub {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise((resolve) => resolve('hashed_password'))
-      }
-    }
+    const { sut, encrypterSub } = makeSut()
 
-    const encryptStub = new EncryterStub()
-    const sut = new DbAddAcount(encryptStub)
-    const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+    const encryptSpy = jest.spyOn(encrypterSub, 'encrypt')
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
